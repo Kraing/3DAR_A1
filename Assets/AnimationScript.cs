7847787f54b7a3142752677288957b4e8ba62530
Carlo Facchin <carlo1996a@gmail.com>
2021-01-08 10:45:05 +0100

    Openpanel script fix

---
+++ Assets/AnimationScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationScript : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public void LoadNextLevel()
+    {
+        Debug.Log("prima coroutine");
+    	StartCoroutine(LoadLevel());
+        Debug.Log("dopo coroutine");
+    }
+
+    public IEnumerator LoadLevel()
+    {
+        Debug.Log("inizio?");
+        transition.SetTrigger("triggerino2");
+        yield return new WaitForSeconds(transitionTime);
+        Debug.Log("set?");	
+
+        //Debug.Log("set?");
+    }
+}
